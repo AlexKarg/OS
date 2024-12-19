918d349fd80e6a88b448e8a12878492d85919fbf
Kargalex <kargalex@ece.auth.gr>
2024-12-19 13:21:39 +0200

    Add Lab05

---
+++ Lab05/example3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int arr[5] = {10, 20, 30, 40, 50};
+	int *p = arr;
+
+	for(int i=0; i<5; i++) {
+		printf("arr[%d]=%d\n", i, arr[i]);
+	}
+
+	for(int i=0; i<5l; i++) {
+		printf("*(p+%d) = %d\n", i, *(p+i));
+	}
+
+	return 0;
+}
